901a827c4edeb55940a9b019980ad66b4f626fb5
Sameer Khan A <153911733+sameerkhan2003a@users.noreply.github.com>
2024-08-06 17:49:01 +0530

    Add files via upload

---
+++ Sameer_Khan/Sameer_Khan/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Sameer_Khan project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sameer_Khan.settings')
+
+application = get_asgi_application()
---
+++ Sameer_Khan/Sameer_Khan/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Sameer_Khan project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sameer_Khan.settings')
+
+application = get_wsgi_application()
